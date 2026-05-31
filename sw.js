94c01b68d0cdde03524c592d285c8ef6d3e205f4
placardofutebol <placardofutebol.com@gmail.com>
2026-05-31 12:41:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,5 +9,8 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) {
+    return;
+  }
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
